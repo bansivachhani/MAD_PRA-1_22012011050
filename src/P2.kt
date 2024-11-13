8432dd0c477d72e3d7b3c16e4e960297624119e2
Bansi <bansivachhani153@gmail.com>
2024-11-13 17:34:30 +0530

    Initial commit

---
+++ src/P2.kt
@@ -0,0 +1,11 @@
+fun main() {
+    var a: Int = 4
+    println("Integer value: $a")
+    var b: Double = a.toDouble()
+    println("Double value from Int: $b")
+    var c: String = "10"
+    println("String value: $c")
+    var d: Int = c.toInt()
+    println("Integer value from String: $d")
+    var e: Double = c.toDouble()
+    println("Double value from String: $e")}
